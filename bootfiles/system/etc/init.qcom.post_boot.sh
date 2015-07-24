d84534a1b75718a7725563d2e468eefd4b4a9067
Electry <electry@X230>
2015-07-25 00:28:58 +0200

    ramdisk: Disabled MP-Decision, system: Modified post_boot sh script

---
+++ bootfiles/system/etc/init.qcom.post_boot.sh
@@ -0,0 +1,115 @@
+#!/system/bin/sh
+# Copyright (c) 2012-2013, The Linux Foundation
+#		     2015, Electry
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of The Linux Foundation nor
+#       the names of its contributors may be used to endorse or promote
+#       products derived from this software without specific prior written
+#       permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+
+echo 4 > /sys/module/lpm_levels/enable_low_power/l2
+echo 1 > /sys/module/msm_pm/modes/cpu0/power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu1/power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu2/power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu3/power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu0/power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu1/power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu2/power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu3/power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu0/standalone_power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu1/standalone_power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu2/standalone_power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu3/standalone_power_collapse/suspend_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu0/standalone_power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu1/standalone_power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu2/standalone_power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu3/standalone_power_collapse/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu0/retention/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu1/retention/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu2/retention/idle_enabled
+echo 1 > /sys/module/msm_pm/modes/cpu3/retention/idle_enabled
+echo 0 > /sys/module/msm_thermal/core_control/enabled
+echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 1 > /sys/devices/system/cpu/cpu2/online
+echo 1 > /sys/devices/system/cpu/cpu3/online
+for devfreq_gov in /sys/class/devfreq/qcom,cpubw*/governor
+do
+    echo "cpubw_hwmon" > $devfreq_gov
+done
+echo 20 > /sys/module/cpu_boost/parameters/boost_ms
+echo 1190400 > /sys/module/cpu_boost/parameters/sync_threshold
+echo 1728000 > /sys/module/cpu_boost/parameters/input_boost_freq
+echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
+setprop ro.qualcomm.perf.cores_online 1
+
+echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 300000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+echo 300000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+echo 300000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+
+chown -h system /sys/devices/system/cpu/cpu0/rq-stats/mpctl
+chmod -h 664    /sys/devices/system/cpu/cpu0/rq-stats/mpctl
+echo 1 > /sys/module/msm_thermal/core_control/enabled
+chown -h root.system /sys/devices/system/cpu/mfreq
+chmod -h 220 /sys/devices/system/cpu/mfreq
+chown -h root.system /sys/devices/system/cpu/cpu1/online
+chown -h root.system /sys/devices/system/cpu/cpu2/online
+chown -h root.system /sys/devices/system/cpu/cpu3/online
+chmod -h 664 /sys/devices/system/cpu/cpu1/online
+chmod -h 664 /sys/devices/system/cpu/cpu2/online
+chmod -h 664 /sys/devices/system/cpu/cpu3/online
+echo 1 > /dev/cpuctl/apps/cpu.notify_on_migrate
+
+emmc_boot=`getprop ro.boot.emmc`
+case "$emmc_boot"
+    in "true")
+        chown -h system /sys/devices/platform/rs300000a7.65536/force_sync
+        chown -h system /sys/devices/platform/rs300000a7.65536/sync_sts
+        chown -h system /sys/devices/platform/rs300100a7.65536/force_sync
+        chown -h system /sys/devices/platform/rs300100a7.65536/sync_sts
+    ;;
+esac
+
+# MP-Decision by qcom
+#start mpdecision
+
+echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
+
+# Let kernel know our image version/variant/crm_version
+image_version="10:"
+image_version+=`getprop ro.build.id`
+image_version+=":"
+image_version+=`getprop ro.build.version.incremental`
+image_variant=`getprop ro.product.name`
+image_variant+="-"
+image_variant+=`getprop ro.build.type`
+oem_version=`getprop ro.build.version.codename`
+echo 10 > /sys/devices/soc0/select_image
+echo $image_version > /sys/devices/soc0/image_version
+echo $image_variant > /sys/devices/soc0/image_variant
+echo $oem_version > /sys/devices/soc0/image_crm_version
+
+# Start RIDL/LogKit II client
+su -c /system/vendor/bin/startRIDL.sh
